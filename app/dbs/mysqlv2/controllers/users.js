9a808b07594a5e4465720c737b8b486a571e5b3a
FdezI <inakitinajo@gmail.com>
2015-07-30 12:15:38 +0100

    Cambios en el perfil

--- app/dbs/mysqlv2/controllers/users.js
+++ app/dbs/mysqlv2/controllers/users.js
@@ -1,10 +1,9 @@
-var mysql = require('mysql');
+var pool = require('mysql').pool;
 
 // UNREGISTERED
 exports.create = function(req, res) {
-	console.log("-----------------------------------------");
 	console.log(req.body);
-	mysql.pool.query('INSERT INTO user SET ?', req.body, function(err, result) {
+	pool.query('INSERT INTO user SET ?', req.body, function(err, result) {
 		if(err) throw err;
 		
 		res.json({id:result.insertId});
@@ -12,7 +11,7 @@ exports.create = function(req, res) {
 };
 
 exports.getAll = function(req, res) {
-	mysql.pool.query('SELECT iduser, name, surnames, avatar, karma FROM user', function(err, rows, fields) {
+	pool.query('SELECT iduser, name, surnames, avatar, karma FROM user', function(err, rows, fields) {
 		if(err) {
 			throw err;
 			//TODO handle errors on server.js
@@ -28,14 +27,11 @@ exports.getAll = function(req, res) {
 exports.get = function(req, res) {
 	if(!('exp' in req.query)) {
 
-		var identifiers = req.query;
-		var user = req.params.iduser;
-		if(Object.keys(req.query).length == 0) {
-			console.log(1);
-			identifiers = ['iduser', 'name', 'surnames', 'email', 'phone', 'carnet', 'avatar', 'birth', 'karma'];
-		} else identifiers = req.query;
+		if(Object.keys(req.query).length == 0)
+			var identifiers = 'iduser, name, surnames, email, phone, carnet = 1 as carnet, avatar, birth, karma';
+		else identifiers = pool.escapeId(req.query);
 		
-		mysql.pool.query('SELECT ??, karma IS NOT NULL AS driver FROM user WHERE user.iduser=?', [identifiers, user], function(err, rows, fields) {
+		pool.query('SELECT ' + identifiers + ', karma IS NOT NULL AS driver FROM user WHERE user.iduser=?', req.params.iduser, function(err, rows, fields) {
 			if(err) throw err;
 
 			res.json(rows[0]);
@@ -90,6 +86,7 @@ exports.getTrips = function(req, res) {
 
 	// FIRST METHOD
 
+	var user = req.params.iduser;
 	var query = '(SELECT LA.city AS departure, LB.city arrival, PA.date as dtime, false as driving\
 											FROM userTrips\
 												LEFT JOIN tripPoints AS PA ON userTrips.trip = PA.trip AND pointA = PA.order\
@@ -107,7 +104,7 @@ exports.getTrips = function(req, res) {
 											WHERE trip.driver=?)';
 
 	// This method is returning: [{departure: '', arrival: '', dtime: null, driving: 0}, {...}]
-	mysql.pool.query(query, [req.params.iduser, req.params.iduser], function(err, rows, result) {
+	pool.query(query, [user, user], function(err, rows, result) {
 
 		if(err) throw err;
 		
@@ -116,7 +113,7 @@ exports.getTrips = function(req, res) {
 };
 
 exports.getAssessments = function(req, res) {
-	mysql.pool.query('SELECT a.*, avatar FROM trip JOIN assessment AS a ON a.trip = idtrip\
+	pool.query('SELECT a.*, avatar FROM trip JOIN assessment AS a ON a.trip = idtrip\
 											JOIN user ON iduser = user\
 										WHERE trip.driver = ?', req.params.iduser, function(err, rows, result) {
 		if(err) throw err;
@@ -128,7 +125,7 @@ exports.getAssessments = function(req, res) {
 
 // AUTHENTICATED
 exports.update = function(req, res) {
-	mysql.pool.query('UPDATE user SET ? WHERE iduser=?', [req.body, req.params.iduser], function(err, result) {
+	pool.query('UPDATE user SET ? WHERE iduser=?', [req.body, req.params.iduser], function(err, result) {
 		if(err) throw err;
 		
 		res.json({changed:result.changedRows});
@@ -136,7 +133,7 @@ exports.update = function(req, res) {
 };
 
 exports.delete = function(req, res) {
-	mysql.pool.query('DELETE FROM user WHERE iduser=?', req.params.iduser, function(err, result) {
+	pool.query('DELETE FROM user WHERE iduser=?', req.params.iduser, function(err, result) {
 		if(err) throw err;
 		
 		res.json({deleted:result.affectedRows});
